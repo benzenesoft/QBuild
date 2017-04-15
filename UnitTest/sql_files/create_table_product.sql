3ac32102198c15ae7597bf5098de2cf70e64da02
mohayemin <mohayemin@localhost>
2017-04-15 14:33:14 +0600

    SQL.CreateDbCommand tested with inmemory sqlite database

---
+++ UnitTest/sql_files/create_table_product.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE product(
+   id INT PRIMARY KEY     NOT NULL,
+   name           TEXT    NOT NULL,
+   price          INT     NOT NULL
+);
+
+INSERT INTO product (id, name, price)
+	VALUES 
+		(1, "almira", 1000),
+		(2,  "table",  500)
