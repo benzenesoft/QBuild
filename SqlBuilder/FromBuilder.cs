55d3cc3b828962b25f058af790e37cf86aa93bd1
mohayemin <mohayemin@localhost>
2017-04-15 10:25:15 +0600

    FromBuilder implemented

---
+++ SqlBuilder/FromBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace BenzeneSoft.SqlBuilder
+{
+    public class FromBuilder<T> : IFromBuilder<T>
+    {
+        private readonly INameResolver _nameResolver;
+        private string _from;
+
+        public FromBuilder(INameResolver nameResolver)
+        {
+            _nameResolver = nameResolver;
+        }
+
+        public ISql Build()
+        {
+            return new Sql(_from);
+        }
+
+        public IFromBuilder From(string fromExpression)
+        {
+            _from = fromExpression;
+            return this;
+        }
+
+        public IFromBuilder<T> Default()
+        {
+            _from = _nameResolver.Table(typeof(T));
+            return this;
+        }
+    }
+}
