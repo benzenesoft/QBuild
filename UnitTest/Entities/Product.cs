217719969df48a0e000edd157bfef55fadcb8e8f
mohayemin <mohayemin@localhost>
2017-04-15 14:58:10 +0600

    LowerSnakeCaseNameResolver added and tested

---
+++ UnitTest/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTest.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public bool IsInStock { get; set; }
+    }
+}
