8c7bd0ddd041b2d466623d172f90e68d1f4bcf56
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2017-04-17 21:55:57 +0600

    tests refactored

---
+++ UnitTest/Doubles/TestConnection.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+namespace UnitTest.Doubles
+{
+    public class TestConnection : IDbConnection
+    {
+        private IDbConnection _connection;
+
+        public TestConnection()
+        {
+            _connection = new SQLiteConnection("Data Source=:memory:");
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        public IDbTransaction BeginTransaction()
+        {
+            return _connection.BeginTransaction();
+        }
+
+        public IDbTransaction BeginTransaction(IsolationLevel il)
+        {
+            return _connection.BeginTransaction(il);
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+
+        public void ChangeDatabase(string databaseName)
+        {
+            _connection.ChangeDatabase(databaseName);
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            return _connection.CreateCommand();
+        }
+
+        public void Open()
+        {
+            _connection.Open();
+            using (var createTableCommand = _connection.CreateCommand())
+            {
+                createTableCommand.CommandText = File.ReadAllText("sql_files/create_table_product.sql");
+                createTableCommand.ExecuteNonQuery();
+            }
+        }
+
+        public string ConnectionString
+        {
+            get { return _connection.ConnectionString; }
+            set { _connection.ConnectionString = value; }
+        }
+
+        public int ConnectionTimeout => _connection.ConnectionTimeout;
+
+        public string Database => _connection.Database;
+
+        public ConnectionState State => _connection.State;
+    }
+}
