a871421d209fdd1ccfea5e81afce060deb1ed4f1
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2017-04-17 23:14:37 +0600

    NonGeneric methods are hidden with explicit implementation. Generic equivalent methords exposed.

--- UnitTest/SelectBuilderTest.cs
+++ UnitTest/SelectBuilderTest.cs
@@ -34,14 +34,5 @@ namespace UnitTest
             var sql = sb.Columns(p => p.Id, p => p.Name).Build();
             Assert.AreEqual("SELECT id\n,name", sql.SqlText);
         }
-
-        [Test]
-        public void Column_Custom_ReplacesOthers()
-        {
-            var sb = new SelectBuilder<Product>(new LowerSnakeCaseNameResolver());
-            
-            var sql = sb.Columns(p => p.Id, p => p.Name).Columns("is_in_stock").Custom(new Sql("whatever")).Build();
-            Assert.AreEqual("whatever", sql.SqlText);
-        }
     }
 }
