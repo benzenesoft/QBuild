bd0c34515992b1d389912c13208886006318a319
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2017-04-22 19:59:57 +0600

    predicate parser added

---
+++ UnitTest/Expressions/PredicateParserTest.cs
@@ -0,0 +1,45 @@
+﻿using BenzeneSoft.QBuild;
+using BenzeneSoft.QBuild.Expressions;
+using NUnit.Framework;
+using UnitTest.Doubles;
+using UnitTest.Entities;
+using static NUnit.Framework.Assert;
+
+namespace UnitTest.Expressions
+{
+    [TestFixture]
+    public class PredicateParserTest
+    {
+        private PredicateParser _parser;
+        private TestConnection _connection;
+        [SetUp]
+        public void Setup()
+        {
+            _parser = new PredicateParser(new LowerSnakeCaseNameResolver(), new OperatorResolver());
+            _connection = new TestConnection();
+            _connection.Open();
+        }
+
+        [Test]
+        public void Constant()
+        {
+            var predicate = _parser.Parse<Product>(product => 1 == 1);
+            var sql = new Sql("select * from product where ").Append(predicate).Append(" order by name");
+
+            var reader = _connection.Read(sql);
+            IsTrue(reader.Read());
+            AreEqual("almira", reader["name"]);
+        }
+
+        [Test]
+        public void SimpleBinary()
+        {
+            var predicate = _parser.Parse<Product>(product => product.Name == "bed");
+            var sql = new Sql("select * from product where ").Append(predicate).Append(" order by name");
+
+            var reader = _connection.Read(sql);
+            IsTrue(reader.Read());
+            AreEqual("bed", reader["name"]);
+        }
+    }
+}
