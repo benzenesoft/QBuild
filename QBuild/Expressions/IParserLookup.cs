6eeb9569d75b9938b6f3e2bc7ffdc60fe9f8e4d2
mohayemin <mohayemin@localhost>
2017-04-23 00:43:15 +0600

    ParserLookup added

---
+++ QBuild/Expressions/IParserLookup.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace BenzeneSoft.QBuild.Expressions
+{
+    public interface IParserLookup
+    {
+        IExpressionParser FindParser(Expression expression);
+    }
+}
