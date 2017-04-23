92fbb774f55b71fc2c85002310fbeddf8906a8a3
mohayemin <mohayemin@localhost>
2017-04-24 00:03:57 +0600

    Logical Or and And supported

--- UnitTest/Expressions/BinaryExpressionParserTest.cs
+++ UnitTest/Expressions/BinaryExpressionParserTest.cs
@@ -5,6 +5,7 @@ using BenzeneSoft.QBuild.Expressions;
 using NUnit.Framework;
 using UnitTest.Doubles;
 using UnitTest.Entities;
+using static NUnit.Framework.Assert;
 
 namespace UnitTest.Expressions
 {
@@ -26,22 +27,66 @@ namespace UnitTest.Expressions
         public void Parse_ColumnColum()
         {
             Expression<Predicate<Product>> exp = p => p.DiscountedPrice < p.Price;
-            var predicate = _parser.Parse((BinaryExpression) exp.Body);
+            var predicate = _parser.Parse((BinaryExpression)exp.Body);
             var sql = new Sql("select * from product where ").Append(predicate);
             var reader = _connection.Read(sql);
-            Assert.IsTrue(reader.Read());
-            Assert.AreEqual("sofa", reader["name"]);
+            IsTrue(reader.Read());
+            AreEqual("sofa", reader["name"]);
         }
 
         [Test(Description = "find cheap items")]
         public void Parse_ColumnValue()
         {
             Expression<Predicate<Product>> exp = p => p.Price <= 15;
-            var predicate = _parser.Parse((BinaryExpression) exp.Body);
+            var predicate = _parser.Parse((BinaryExpression)exp.Body);
             var sql = new Sql("select * from product where ").Append(predicate);
             var reader = _connection.Read(sql);
-            Assert.IsTrue(reader.Read());
-            Assert.AreEqual("bench", reader["name"]);
+            IsTrue(reader.Read());
+            AreEqual("bench", reader["name"]);
+        }
+
+        [Test(Description = "cheap or has discount")]
+        public void Parse_Or()
+        {
+            Expression<Predicate<Product>> exp = p => (p.Price <= 15) || (p.DiscountedPrice < p.Price);
+
+            var eBody = exp.Body;
+            var predicate = _parser.Parse(eBody);
+
+            var sql = new Sql("select * from product where ").Append(predicate).Append(" order by price");
+            var reader = _connection.Read(sql);
+
+            IsTrue(_parser.CanParse(eBody));
+
+            IsTrue(reader.Read());
+            AreEqual("bench", reader["name"]);
+
+            IsTrue(reader.Read());
+            AreEqual("sofa", reader["name"]);
+
+            IsTrue(reader.Read());
+            AreEqual("sofa", reader["name"]);
+
+            IsFalse(reader.Read());
+        }
+
+        [Test(Description = "expensive and has discount")]
+        public void Parse_And()
+        {
+            Expression<Predicate<Product>> exp = p => (p.Price >= 65) && (p.DiscountedPrice < p.Price);
+
+            var eBody = exp.Body;
+            var predicate = _parser.Parse(eBody);
+
+            var sql = new Sql("select * from product where ").Append(predicate).Append(" order by price");
+            var reader = _connection.Read(sql);
+
+            IsTrue(_parser.CanParse(eBody));
+
+            IsTrue(reader.Read());
+            AreEqual("sofa", reader["name"]);
+
+            IsFalse(reader.Read());
         }
     }
 }
