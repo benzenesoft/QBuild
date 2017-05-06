095cf26c35ca064bda556aa85e186750065cf916
mohayemin <mohayemin@localhost>
2017-05-06 23:38:22 +0600

    LambdaQueryBuilder added

---
+++ QBuild/Builders/LambdaQueryBuilder.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq.Expressions;
+using BenzeneSoft.QBuild.Clauses;
+using BenzeneSoft.QBuild.Expressions;
+using BenzeneSoft.QBuild.NameResolvers;
+
+namespace BenzeneSoft.QBuild.Builders
+{
+    public class LambdaQueryBuilder : IClauseBuilder
+    {
+        private readonly ILambdaParser _parser;
+        private readonly INameResolver _nameResolver;
+        private SelectClause _select;
+        private FromClause _from;
+        private IClause _where;
+        private GroupByClause _groupBy;
+        private IClause _having;
+        private OrderByClause _orderBy;
+
+        public LambdaQueryBuilder(ILambdaParser parser, INameResolver nameResolver)
+        {
+            _parser = parser;
+            _nameResolver = nameResolver;
+            _select = new SelectClause();
+            _from = new FromClause();
+            _groupBy = new GroupByClause();
+            _orderBy = new OrderByClause();
+        }
+
+        public IClause Build()
+        {
+            var queryBuilder = new RawQueryBuilder()
+                .Select(_select)
+                .From(_from)
+                .Where(_where)
+                .GroupBy(_groupBy)
+                .Having(_having)
+                .OrderBy(_orderBy);
+
+            return queryBuilder.Build();
+        }
+
+        public LambdaQueryBuilder SelectAll()
+        {
+            _select.All();
+            return this;
+        }
+
+        public LambdaQueryBuilder SelectAs<T>(Expression<Func<T, object>> expression, string alias)
+        {
+            _select.ColumnAs(_parser.Parse(expression), new Clause(alias));
+            return this;
+        }
+
+        public LambdaQueryBuilder Select<T>(params Expression<Func<T, object>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                _select.Column(_parser.Parse(expression));
+            }
+            return this;
+        }
+
+        public LambdaQueryBuilder From<T>()
+        {
+            _from.Table(_nameResolver.Table(typeof(T)));
+            return this;
+        }
+
+        public LambdaQueryBuilder Where<T>(Expression<Func<T, bool>> predicate)
+        {
+            _where = _parser.Parse(predicate);
+            return this;
+        }
+
+        public LambdaQueryBuilder GroupBy<T>(params Expression<Func<T, object>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                _groupBy.Column(_parser.Parse(expression));
+            }
+            return this;
+        }
+
+        public LambdaQueryBuilder Having<T>(Expression<Func<T, bool>> predicate)
+        {
+            _having = _parser.Parse(predicate);
+            return this;
+        }
+
+        public LambdaQueryBuilder OrderByAsc<T>(Expression<Func<T, object>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                _orderBy.Asc(_parser.Parse(expression));
+            }
+            return this;
+        }
+
+        public LambdaQueryBuilder OrderByDesc<T>(Expression<Func<T, object>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                _orderBy.Desc(_parser.Parse(expression));
+            }
+            return this;
+        }
+    }
+}
---
+++ QBuild/Utils/ClauseExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using BenzeneSoft.QBuild.Clauses;
+
+namespace BenzeneSoft.QBuild.Utils
+{
+    public static class ClauseExtensions
+    {
+        public static bool IsEmpty(this IClause clause)
+        {
+            return string.IsNullOrEmpty(clause.Text) && !clause.Parameters.Any();
+        }
+    }
+}
