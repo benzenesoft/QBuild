c9198ca31964605822171adfbc7b95259c668b6e
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2017-05-06 08:05:15 +0600

    GroupByClause added

---
+++ QBuild/Clauses/GroupByClause.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace BenzeneSoft.QBuild.Clauses
+{
+    public class GroupByClause : IClause
+    {
+        public string Text => _delegate.Text;
+        public IEnumerable<Parameter> Parameters => _delegate.Parameters;
+
+        private readonly SeparatedClause _delegate;
+
+        public GroupByClause()
+        {
+            _delegate = new SeparatedClause(new MutableClause().Line().Append(","));
+        }
+
+        public GroupByClause Column(string expression)
+        {
+            _delegate.AppendSeparated(expression);
+            return this;
+        }
+
+        public GroupByClause Column(IClause expression)
+        {
+            _delegate.AppendSeparated(expression);
+            return this;
+        }
+    }
+}
