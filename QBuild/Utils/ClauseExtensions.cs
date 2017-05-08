e01d5c8720e2a68aa18913b4f7944e23476b1a03
mohayemin <mohayemin@localhost>
2017-05-08 09:00:08 +0600

    extension methods added for IClause and Parameter to create dbcommand and dbparam

--- QBuild/Utils/ClauseExtensions.cs
+++ QBuild/Utils/ClauseExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data;
+using System.Linq;
 using BenzeneSoft.QBuild.Clauses;
 
 namespace BenzeneSoft.QBuild.Utils
@@ -8,6 +9,19 @@ namespace BenzeneSoft.QBuild.Utils
         public static bool IsEmpty(this IClause clause)
         {
             return string.IsNullOrEmpty(clause.Text) && !clause.Parameters.Any();
+        }
+
+        public static IDbCommand ToDbCommand(this IClause clause, IDbConnection connection)
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = clause.Text;
+            foreach (var parameter in clause.Parameters)
+            {
+                var param = parameter.ToDbParameter(command);
+                command.Parameters.Add(param);
+            }
+
+            return command;
         }
     }
 }
---
+++ QBuild/Utils/ParameterExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using BenzeneSoft.QBuild.Clauses;
+
+namespace BenzeneSoft.QBuild.Utils
+{
+    public static class ParameterExtensions
+    {
+        public static IDbDataParameter ToDbParameter(this Parameter parameter, IDbCommand command)
+        {
+            var dbParameter = command.CreateParameter();
+            dbParameter.ParameterName = parameter.Name;
+            dbParameter.Value = parameter.Value;
+            return dbParameter;
+        }
+    }
+}
