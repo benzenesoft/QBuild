a2664f78aad4d4877da1476dd8966352753f21c4
mohayemin <mohayemin@localhost>
2017-05-09 23:34:15 +0600

    AsIsNameResolver added

---
+++ QBuild/NameResolvers/AsIsNameResolver.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace BenzeneSoft.QBuild.NameResolvers
+{
+    public class AsIsNameResolver : INameResolver
+    {
+        public string Resolve(MemberInfo memberInfo)
+        {
+            return memberInfo.Name;
+        }
+    }
+}
