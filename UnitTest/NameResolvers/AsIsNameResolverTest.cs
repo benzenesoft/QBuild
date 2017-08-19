b456b69c5895539f879ada41c28c043fe262b602
mohayemin <mohayemin@localhost>
2017-08-19 12:35:02 +0600

    AsIsNameResolver tested

---
+++ UnitTest/NameResolvers/AsIsNameResolverTest.cs
@@ -0,0 +1,18 @@
+﻿using BenzeneSoft.QBuild.NameResolvers;
+using NUnit.Framework;
+using UnitTest.Doubles;
+
+namespace UnitTest.NameResolvers
+{
+    [TestFixture]
+    public class AsIsNameResolverTest
+    {
+        [Test]
+        public void Resolve()
+        {
+            var resolver = new AsIsNameResolver();
+            Assert.AreEqual(nameof(ProductTag), resolver.Resolve(typeof(ProductTag)));
+            Assert.AreEqual(nameof(ProductTag.ProductId), resolver.Resolve(typeof(ProductTag).GetProperty("ProductId")));
+        }
+    }
+}
